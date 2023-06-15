0eac5317ffb386fbc2f6525f9bbd0d01c3ad79c3
cretara <cretara>
2023-06-15 14:29:58 +0200

    Typed fields in app.component Fixed Google social login button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,28 @@
-import { SocialAuthService } from '@abacritt/angularx-social-login';
-import { Component } from '@angular/core';
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-social-login';
-  user:any;
-  loggedIn:any;
-  constructor(private authService: SocialAuthService) { }
+export class AppComponent implements OnInit, OnDestroy {
+  user!: SocialUser;
+  loggedIn: boolean = false;
+  private socialAuthServiceSubscription: Subscription = new Subscription();
+
+  constructor(private socialAuthService: SocialAuthService) {}
+
+  ngOnDestroy(): void {
+    this.socialAuthServiceSubscription.unsubscribe();
+  }
 
   ngOnInit() {
-    this.authService.authState.subscribe((user) => {
-      this.user = user;
-      this.loggedIn = (user != null);
-      console.log(this.user)
-    });
+    this.socialAuthServiceSubscription =
+      this.socialAuthService.authState.subscribe((userResponse) => {
+        this.user = userResponse;
+        this.loggedIn = this.user.authToken != null;
+      });
   }
 }
